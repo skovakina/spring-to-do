c823ece784afd5050dd85717291d6bfca646c26d
Svetlana Kovakina <skovakina@gmail.com>
2024-02-28 20:31:04 -0800

    feat: add current date

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,20 @@ import { getWeather } from "../utils/weatherApi";
 getWeather()
   .then((weatherData) => {
     console.log(weatherData);
-    // Do something with the UI
+    // weatherData.weather[0].icon;
+    // const iconUrl = `https://openweathermap.org/img/wn/${weatherCode}.png`;
+    // weatherElement.src = iconUrl;
   })
   .catch((error) => {
     console.error("Error:", error);
   });
+
+const todayElement = document.querySelector(".main__info-text");
+const weatherElement = document.querySelector(".main__info-weather-icon");
+const date = new Date();
+
+todayElement.textContent = date.toLocaleString("en-US", {
+  weekday: "short",
+  month: "short",
+  day: "numeric",
+});
